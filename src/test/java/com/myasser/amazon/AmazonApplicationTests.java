ebc8dca77df7fa9dfb3fba2260a6e38ef13aa57f
Mohammed Yasser <mohammedre4a@gmail.com>
2022-09-25 14:37:23 +0200

    Initial commit

---
+++ src/test/java/com/myasser/amazon/AmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myasser.amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
