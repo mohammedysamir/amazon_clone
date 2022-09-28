3304000e53459d6adc0b4581f776a471e3132432
mohammed yasser <47453799+mohammedysamir@users.noreply.github.com>
2022-09-28 22:36:10 +0200

    Merge pull request #3
    * refactor: added @Setter and @Getter from lombok to clean up the code …

--- src/main/java/com/myasser/amazon/model/User.java
+++ src/main/java/com/myasser/amazon/model/User.java
@@ -1,62 +1,34 @@
 package com.myasser.amazon.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("users")
 public class User {
     @Id
+    @Setter
+    @Getter
     @JsonProperty("id")
     String userId;
+    @Setter
+    @Getter
     @JsonProperty("name")
     String name;
+    @Setter
+    @Getter
     @JsonProperty("email")
     String email;
+    @Setter
+    @Getter
     @JsonProperty("password")
     String password;
+    @Setter
+    @Getter
     @JsonProperty("cart")
     Cart cart;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Cart getCart() {
-        return cart;
-    }
-
-    public void setCart(Cart cart) {
-        this.cart = cart;
-    }
 
     public User(String name, String email, String password, Cart cart) {
         this.name = name;
